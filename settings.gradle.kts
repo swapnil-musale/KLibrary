6459b20a49315f389eda4ffbf51b7b3ec212c76d
Swapnil Musale <swapnilmusale19@gmail.com>
2024-01-25 19:11:08 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "KLibrary"
+include(":kLibrary")
+includeBuild("convention-plugins")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
